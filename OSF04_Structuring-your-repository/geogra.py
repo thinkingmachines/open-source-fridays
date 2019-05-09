fd68eaf3f5d31189fd7be68c567d51ca29d6f9b3
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-09 23:55:54 +0800

    Add get-distance command to geogra.py

--- OSF04_Structuring-your-repository/geogra.py
+++ OSF04_Structuring-your-repository/geogra.py
@@ -6,9 +6,15 @@
 import click
 import pandas as pd
 from geomancer.spellbook import SpellBook
+from geomancer.spells import DistanceToNearest
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+
+@main.command()
 @click.argument("input_path", type=click.Path(exists=True))
 @click.argument("output_path", type=click.Path())
 @click.option(
@@ -24,5 +30,38 @@ def extract_features(input_path, output_path, spellbook):
     print(output_df)
 
 
+@main.command()
+@click.argument("input_path", type=click.Path(exists=True), nargs=1)
+@click.argument("output_path", type=click.Path(), nargs=1)
+@click.argument("pois", type=str, nargs=-1)
+@click.option(
+    "--spellbook-path",
+    help="save SpellBook to JSON path",
+    type=click.Path(),
+    default=None,
+)
+def get_distance(input_path, output_path, pois, spellbook_path):
+    """Get distance given a list of POIs"""
+    spells = [
+        DistanceToNearest(
+            poi,
+            feature_name="dist_{}".format(poi),
+            dburl="sqlite:///data/source.sqlite",
+            source_table="gis_osm_pois_free_1",
+        )
+        for poi in pois
+    ]
+
+    spellbook = SpellBook(spells)
+    input_df = pd.read_csv(input_path)
+    output_df = spellbook.cast(input_df)
+    output_df.to_csv(output_path, index=False)
+    print(output_df)
+
+    if spellbook_path:
+        spellbook.to_json(spellbook_path)
+        print("Saved to {}".format(spellbook_path))
+
+
 if __name__ == "__main__":
-    extract_features()
+    main()
