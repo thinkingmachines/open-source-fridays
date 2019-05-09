a2c93a7857022ec55f2b469c57d45a7508cd477d
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-05-09 22:09:07 +0800

    Update parameter signature of method

--- OSF04_Structuring-your-repository/geogra.py
+++ OSF04_Structuring-your-repository/geogra.py
@@ -9,18 +9,18 @@ from geomancer.spellbook import SpellBook
 
 
 @click.command()
-@click.argument("input", type=click.Path(exists=True))
-@click.argument("output", type=click.Path())
+@click.argument("input_path", type=click.Path(exists=True))
+@click.argument("output_path", type=click.Path())
 @click.option(
     "--spellbook", help="path to SpellBook", type=click.Path(exists=True)
 )
-def extract_features(**opts):
+def extract_features(input_path, output_path, spellbook):
     """Create new features given a geomancer Spellbook"""
 
-    input_df = pd.read_csv(opts["input"])
-    spellbook = SpellBook.read_json(opts["spellbook"])
+    input_df = pd.read_csv(input_path)
+    spellbook = SpellBook.read_json(spellbook)
     output_df = spellbook.cast(input_df)
-    output_df.to_csv(opts["output"], index=False)
+    output_df.to_csv(output_path, index=False)
     print(output_df)
 
 
